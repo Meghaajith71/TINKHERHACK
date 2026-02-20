693a5f4c92a0d371b5ed298a06b99f99b4001022
Anju Prakash <anjuprakash79205@gmail.com>
2026-02-21 01:45:59 +0530

    edit meal

--- script.js
+++ script.js
@@ -296,9 +296,8 @@ function generateMenu() {
         }
 
         let lunch = [];
-        if (!isTooSoon("Rice", dayIndex, lastDayByItem)) {
-            lunch.push("Rice");
-        }
+        // Rice is compulsory for lunch every day
+        lunch.push("Rice");
         let breakfastExtras = [];
         let dinnerExtras = [];
 
